90db81f80473ff06ed0b41424eb0e6e163c9435a
Alexander-Sol <AlexSolivais@gmail.com>
2021-07-25 16:56:14 -0400

    Sequestered manuscript related/text files

--- Manuscript/convertToMarkdown.R
+++ Manuscript/convertToMarkdown.R
@@ -5,11 +5,11 @@ doc.name <- "AutoClustR_Outline"
 
 # doc.name <- readline(prompt = "Enter the path to the .docx file to be converted (do not include the file extension)") 
 
-if(!is.na(file.info(paste0(doc.name, ".markdown")))) {
-  file.remove(paste0(doc.name, ".markdown"))
+if(!is.na(file.info(paste0("Manuscript/", doc.name, ".markdown")))) {
+  file.remove(paste0("Manuscript/", doc.name, ".markdown"))
 }
 
-knitr::pandoc(input = paste0(doc.name, ".docx"),
+knitr::pandoc(input = paste0("Manuscript/", doc.name, ".docx"),
               format = "markdown")
 
 
