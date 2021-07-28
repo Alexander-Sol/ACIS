b95d4e204efbac25fd6a8a437b3db5b723d34147
AlxSully <AlexSolivais@gmail.com>
2021-07-28 11:25:06 -0400

    Final commit from Hashino Lab  PC

--- RScripts/AutoClustR.funs.071321.R
+++ RScripts/AutoClustR.funs.071321.R
@@ -191,7 +191,7 @@ AutoClustR <- function(object,
     }else if(class(object) == "SingleCellExperiment"){
       roots <- metadata(object)$roots
     }
-    n.pcs <- Select.nPC(roots, file.path = file.path, do.plot = F)
+    n.pcs <- Select.nPC(roots, file.path = file.path, do.plot = T)
   }
   
   n.pcs <- n.pcs[[2]]
@@ -258,7 +258,125 @@ source("RScripts/nPC.funs.R")
 b1 <- Baron.to.SCexp("Data/Baron-1/GSM2230757_human1_umifm_counts.csv")
 b1 <- Prep.data(b1)
 b1 <- Proc.data(b1)
-ACTest <- AutoClustR(b1, file.path = "", method = "Nelder-Mead", n.priors = 3, n.starts = 3)
-ac.obj <- FindNeighbors(ACTest[[1]], k.param = ACTest[[4]]$k.param, dims = 1:ACTest[[3]]) %>%
-  FindClusters(resolution = ACTest[[4]]$resolution)
-eval <- Test.method(ac.obj, method = "autoclustr")
+
+kolodz <- Kolodz.to.SCexp("Data/Kolodz/counttable_es.csv")
+kolodz <- Prep.data(kolodz)
+kolodz <- Proc.data(kolodz, expr.meas = "cpm")
+ACTest <- AutoClustR(kolodz,
+                     file.path = "",
+                     method = "Bayesian",
+                     n.priors = 16,
+                     n.starts = 16)
+sc.runtime <- Sys.time()
+sc.test <- subcluster(ACTest)
+sc.runtime <- Sys.time() - sc.runtime
+
+subcluster <- function(ACTest) {
+  ac.obj <- FindNeighbors(ACTest[[1]], k.param = ACTest[[4]]$k.param, dims = 1:ACTest[[3]]) %>%
+    FindClusters(resolution = ACTest[[4]]$resolution)
+  n.pcs <- ACTest[[3]]
+  initial.eval <- Test.method(ac.obj, method = "autoclustr")
+  accept = T
+  
+  while(accept){
+    clusterSilhouettes <- getSilScores(ac.obj, npc = 8)
+    check.clusters <- TRUE
+    cl.iter <- 1
+    while(check.clusters) {
+      sc.object <- subset(ac.obj, idents = clusterSilhouettes[cl.iter, 1])
+      sc.k.param.range <- k.param.range <- c(2,140)
+      if (k.param.range[2] > ncol(sc.object)/2) {sc.k.param.range[2] <- round(ncol(sc.object)/2) }
+      if (sc.k.param.range[2] <= 4) {cl.iter <- cl.iter + 1} else {check.clusters <- FALSE}
+    }
+    x.bounds <- list(k.param = sc.k.param.range, resolution = c(0.0, 2.0))
+    
+    sc.results <- ParBayesianOptimization::bayesOpt(
+      FUN = function(...){ 
+        ClustR(object = sc.object, n.pcs = n.pcs, ...)
+      },
+      iters.n = 8 - 1, 
+      initPoints = 8, 
+      bounds = x.bounds,
+      acq = "ei"
+    )
+    
+    sc.object <- bestBayes(sc.object, sc.results, n.pcs = n.pcs)
+    adjust.output <- adjustIdents(ac.obj, temp.object = sc.object, npc = n.pcs)
+    ac.obj <- adjust.output$object
+    accept <- adjust.output$accept
+  }
+  
+  idents <- as.int.factor(ac.obj@active.ident) # as.int.factor should probably be a class function
+  levels(ac.obj@active.ident) <- 0:sum(1:max(idents) %in% unique(idents))
+  
+  return(
+    list(
+      object = ac.obj,
+      initial.eval = initial.eval,
+      final.eval = Test.method(ac.obj, method = "autoclustr")
+      )
+    )
+}
+
+bestBayes <- function(object, bayes.out, n.pcs) {
+  best.params <- getBestPars(bayes.out)
+  object <- FindNeighbors(object, k.param = best.params$k.param, dims = 1:n.pcs) %>%
+    FindClusters(resolution = best.params$resolution)
+  return(object)
+}
+
+getSilScores <- function(object, npc) {
+  embeds <- Embeddings(object)[ , 1:npc]
+  embed.table <- cbind(embeds,
+                       as.integer(levels(object@active.ident))[object@active.ident])
+  sil.scores <- cluster::silhouette(as.integer(embed.table[ , ncol(embed.table)]),
+                                    dist(embed.table[, 1:(ncol(embed.table-1))]))
+  sil <- data.frame(cluster = sil.scores[,1], si_width = sil.scores[,3])
+  scores.by.cluster <- aggregate(sil$si_width, list(sil$cluster), mean) %>%
+    arrange(x)
+  names(scores.by.cluster) <- c("Seurat_cluster", "avg.SI.score")
+  return(scores.by.cluster)
+}
+
+getICVI <- function(object, nPC = NULL) {
+  icvi <- suppressWarnings(
+    index.S(
+      d = dist(
+        Embeddings(object, reduction = "pca")[, 1:nPC],
+        method = "manhattan"),
+      cl = as.integer(Idents(object))
+      )
+    )
+  if(is.nan(icvi)) {icvi <- 0}
+  return(icvi)
+}
+
+as.int.factor <- function(x) {as.integer(levels(x))[x]}
+
+adjustIdents <- function(object, temp.object, npc) {
+  embeds <- Embeddings(object)[ , 1:npc]
+  new.idents <- as.int.factor(temp.object@active.ident) + 
+    1 + 
+    max(as.int.factor(object@active.ident))
+  names(new.idents) <- Cells(temp.object)
+  combined.new <- as.int.factor(object@active.ident)
+  names(combined.new) <- Cells(object)
+  combined.new[names(new.idents)] <- new.idents
+  original.SI <- getICVI(object, npc)
+  # original.SI <- intCriteria(embeds, as.int.factor(object@active.ident), crit = "sil")[[1]]
+  new.SI <-  suppressWarnings(index.S(d = dist(Embeddings(object, reduction = "pca")[, 1:npc],
+                                               method = "manhattan"),
+                                      cl = combined.new %>% as.factor %>% as.numeric))
+  # new.SI <- intCriteria(embeds, as.integer(combined.new), "sil")[[1]]
+  if(original.SI > new.SI) {
+    print(paste(object@project.name, "REJECTED sub clustering"))
+    print(paste("Original:", original.SI, "New:", new.SI))
+    accept <- FALSE
+  } else if (original.SI <= new.SI) {
+    object@active.ident <- as.factor(combined.new)
+    print(paste(object@project.name, "ACCEPTED sub clustering"))
+    print(paste("Original:", original.SI, "New:", new.SI))
+    accept <- TRUE
+  }
+  return(list(object=object, accept = accept))
+}
